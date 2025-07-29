0e872139f2046db12581bbc0b572c2dbb4237405
Larry Banks <larrykbanks21@gmail.com>
2025-07-28 21:41:12 -0400

    refactor: clones repository into /build then builds program

--- builder/builder.py
+++ builder/builder.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import logging
+from pathlib import Path
 
 logging.basicConfig()
 logger = logging.getLogger(f"builder.{__name__}")  # log format will be [MODULE: MSG]
@@ -9,29 +10,61 @@ logger.setLevel(logging.INFO)
 
 class Builder:
     BUILD_CMD = "make"
+    BUILD_DIR = Path("../build").resolve()
 
     def __init__(self):
         pass
 
-    def build(self, repo) -> bool:
+    def clone_repo(self, repo):
+        """
+        params:
+            repo: string with an expected format of a valid git protocol e.g. git@github.com:some-users/repository
+        raises:
+            OSError
+            CalledProcessError
+        """
+        logger.info("Cloning %s into %s", repo, Builder.BUILD_DIR)
+        try:
+            os.chdir(Builder.BUILD_DIR)
+        except OSError as e:
+            logger.error("Failed to change directory: %s", e.strerror)
+            raise e
+        subprocess.run(["/usr/bin/git", "clone", repo], check=True)
+
+    def build(self, repo):
         """
         Compile C program into a binary
 
         Creates a subprocess
-
-        returns:
-            True -> subprocess returned a status code of zero
-            False -> subprocess returned a non-zero status code
-
         raises:
             OSError
+            CalledProcessError
         """
+        logger.info("Building %s")
         try:
-            os.chdir(repo)
+            os.chdir(Path(Builder.BUILD_DIR, repo.name))
         except OSError as e:
             logger.error("Failed to change directory: %s", e.strerror)
             raise e
-        proc = subprocess.run(Builder.BUILD_CMD)
-        status = proc.returncode
-        logger.info("Process exited with status code %d", status)
-        return status == 0
+        subprocess.run(Builder.BUILD_CMD, check=True)
+
+    def run(self, repo):
+        """
+        Clone and build C program
+
+        params:
+            repo:
+        raises:
+            OSError
+            CalledProcessError
+        """
+        try:
+            self.clone_repo(repo)
+        except Exception as e:
+            logger.error("Failed to clone repo: %s", e)
+            raise e
+        try:
+            self.build(repo)
+        except Exception as e:
+            logger.error("Failed to build program: %s", e)
+            raise e
