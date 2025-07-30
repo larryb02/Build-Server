08235412f3fd9732f75406cf7309cb985f713acf
Larry Banks <larrykbanks21@gmail.com>
2025-07-30 10:41:08 -0400

    refactor: pass logger as part of context

--- api/main.py
+++ api/main.py
@@ -1,12 +1,28 @@
+import logging
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from api.repository.views import router as repository_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    ctx = {}
+    # initialize logger
+    logging.basicConfig()
+    logging.getLogger("uvicorn").handlers.clear()
+    logger = logging.getLogger(f"{__name__}")
+    logger.setLevel(logging.INFO)
+    ctx['logger'] = logger
+    yield ctx
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # temporarily allowing all origins
+    allow_origins=["*"],  # temporarily allowing all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
