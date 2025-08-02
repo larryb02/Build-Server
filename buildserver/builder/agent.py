017edef718e393f2b02b08fe81fac31c704612df
Larry Banks <larrykbanks21@gmail.com>
2025-08-02 15:39:56 -0400

    refactor: Catching async errors

--- buildserver/builder/agent.py
+++ buildserver/builder/agent.py
@@ -57,46 +57,53 @@ class Agent:
         }
 
     async def __build_program(self):
-        job_id, repo_url = await self.build_job_queue.get()
-        self.logger.info(f"[Worker-{job_id}] Building: {repo_url}")
-        b = Builder()
         try:
-            status = b.run(repo_url)
-            try:
-                self.jobs[job_id].result.set_result(status)
-            except asyncio.InvalidStateError as e:
-                self.logger.error(f"Failed to set result of future {e}")
-                raise e
-        except Exception as e:
-            self.logger.error(f"[Worker-{job_id}] Build fail: {e}")
+            job_id, repo_url = await self.build_job_queue.get()
+            async with asyncio.timeout(10):
+                logger.info(f"[Worker-{job_id}] Building: {repo_url}")
+                b = Builder()
+                try:
+                    status = b.run(repo_url)
+                    try:
+                        self.jobs[job_id].result.set_result(status)
+                    except asyncio.InvalidStateError as e:
+                        logger.error(f"Failed to set result of future {e}")
+                        raise e
+                except Exception as e:
+                    logger.error(f"[Worker-{job_id}] Build fail: {e}")
+                    self.jobs[job_id].result.set_exception(e)
+        except TimeoutError as e:
+            logger.error(f"[Worker-{job_id}] timed out")
             self.jobs[job_id].result.set_exception(e)
+            raise e
         # self.jobs[job_id].status = Status.COMPLETED
 
     async def __send_artifacts(self):
         job_id, repo_url = await self.artifact_job_queue.get()
-        self.logger.info(f"[Worker-{job_id}] Gathering artifacts for build: {repo_url}")
+        logger.info(f"[Worker-{job_id}] Gathering artifacts for build: {repo_url}")
         b = Builder()
         try:
             artifacts = b.gather_artifacts(repo_url)
         except Exception as e:
-            self.logger.error(f"[Worker-{job_id}] Job failed: {e}")
+            logger.error(f"[Worker-{job_id}] Job failed: {e}")
             self.jobs[job_id].result.set_exception(e)
+            raise e
         self.jobs[job_id].result.set_result(artifacts)
         # self.jobs[job_id].status = Status.COMPLETED
 
     async def add_job(self, job_type: JobType, job: any) -> UUID:
         job_id = uuid4()
-        self.logger.info(
+        logger.info(
             f"Added new job: [{job_id} {job_type}]: {job} id: {id(self.jobhandlers[job_type]["queue"])}"
         )
         try:
             await self.jobhandlers[job_type]["queue"].put((job_id, job))
         except Exception as e:
-            self.logger.error("Failed to add job to queue")
+            logger.error(f"Failed to add job to queue: {e}")
             raise e
         # self.jobs[job_id].status = Status.QUEUED
         self.jobs[job_id].result = asyncio.get_running_loop().create_future()
-        self.logger.debug(
+        logger.debug(
             f"[{job_type}] Queue Size: {self.jobhandlers[job_type]["queue"].qsize()}"
         )
         return job_id
@@ -104,15 +111,25 @@ class Agent:
     def close(self):
         for w in self.workers:
             w.cancel()
-        self.logger.info("Shut down build agent")
+        logger.info("Shut down build agent")
 
     async def do_job(self, job_type: JobType):
         while True:
-            await self.jobhandlers[job_type]["fn"]()
+            try:
+                await self.jobhandlers[job_type]["fn"]()
+            except Exception as e:
+                logger.error(f"Caught exception {e}")
+            logger.debug("task done")
             self.jobhandlers[job_type]["queue"].task_done()
 
     async def run(self):
-        self.logger.info("Started build agent")
+        logger.info(f"Started build agent: [{id(asyncio.get_running_loop())}]")
         self.workers = [
             asyncio.create_task(self.do_job(job_type)) for job_type in JobType
         ]
+        try:
+            for worker in self.workers:
+                await worker
+        except Exception as e:
+            logger.error(f"Exception raised: {e}")
+            raise
