c817b63bca2e98e6f9e92545961a13e1972cf158
Larry Banks <larrykbanks21@gmail.com>
2025-08-04 11:36:24 -0400

    test: added unit tests for clone_repo and build

--- buildserver/tests/builder_test.py
+++ buildserver/tests/builder_test.py
@@ -1,14 +1,36 @@
-from buildserver.builder.builder import Builder
+from pathlib import Path
 import pytest
 
+import buildserver.builder.builder as builder
+from buildserver import utils
+from buildserver.config import BUILD_DIR
 
-builder = Builder()
 
 def test_build():
-    
     build_repo = "../hello"
-    assert builder.build(build_repo) == True
+    builder.build(build_repo)  # no exceptions should be thrown here
 
     bad_build_repo = "../thisdoesntexist"
     with pytest.raises(OSError):
         builder.build(bad_build_repo)
+
+
+def test_clone():
+    git_repo_url = "git@github.com:rpm-software-management/dnf.git"
+    builder.clone_repo(git_repo_url)
+    # Repository should exist in $BUILD_DIR/repo
+    assert Path(BUILD_DIR, "dnf").exists()
+    utils.cleanup_build_files(Path(BUILD_DIR, "dnf"))
+
+    http_repo_url = "https://github.com/rpm-software-management/dnf.git"
+    builder.clone_repo(http_repo_url)
+    assert Path(BUILD_DIR, "dnf").exists()
+    utils.cleanup_build_files(Path(BUILD_DIR, "dnf"))
+
+
+def test_run():
+    # repo doesnt exist
+
+    # build fail
+    # # build success
+    assert True
