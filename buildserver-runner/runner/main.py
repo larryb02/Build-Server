ff554a288dbac2627cddd0dc382124c3d645e773
Larry Banks <larrykbanks21@gmail.com>
2026-02-09 00:42:41 +0000

    fake version callback

--- buildserver-runner/runner/main.py
+++ buildserver-runner/runner/main.py
@@ -3,23 +3,39 @@ import logging
 import typer
 
 from runner.config import LOG_LEVEL
+from runner.agent import Agent
 
-# import Agent
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(LOG_LEVEL)
 
+
+def version_callback(value: bool):
+    if value:
+        print("buildserver-runner 0.1.0")
+        raise typer.Exit()
+
+
 app = typer.Typer(no_args_is_help=True)
 
 
-@app.command()
-def main(version: bool = typer.Option(None, "--version", "-v")):
-    print(LOG_LEVEL)
+@app.callback()
+def main(
+    version: bool = typer.Option(
+        None,
+        "--version",
+        "-v",
+        callback=version_callback,
+        is_eager=True,
+        help="Show version and exit",
+    ),
+):
+    """Buildserver Runner - execution node for buildserver"""
 
 
-@app.command(name="start", help="start buildserver-runner")
+@app.command(name="start", help="Start buildserver-runner")
 def start_runner():
-    pass
+    Agent().start()
 
 
 if __name__ == "__main__":
