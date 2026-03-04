4aea9f2b692afa1993dda3d2e0383deed1e7f9bc
Larry Banks <larrykbanks21@gmail.com>
2026-03-04 08:28:58 -0800

    correct path

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 python3 -m venv init.venv
 source init.venv/bin/activate
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r .devcontainer/requirements.txt
 ansible-playbook infra/ansible/deploy_buildserver/site.yml --tags dev -e cluster_namespace=devpod
 deactivate
