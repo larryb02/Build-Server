fdc90045496d250f1eb6ad421a46ea19b0640653
Larry Banks <larrykbanks21@gmail.com>
2026-03-04 08:25:10 -0800

    assume this gets called from top level

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -2,5 +2,5 @@
 python3 -m venv init.venv
 source init.venv/bin/activate
 python3 -m pip install ansible
-ansible-playbook ../infra/ansible/deploy_buildserver/site.yml --tags dev -e cluster_namespace=devpod
+ansible-playbook infra/ansible/deploy_buildserver/site.yml --tags dev -e cluster_namespace=devpod
 deactivate
