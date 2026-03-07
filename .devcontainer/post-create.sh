8498a27addd5b4f05e4e4aee50ce627db252e8c3
Larry B <larrykbanks21@gmail.com>
2026-03-07 18:40:42 +0000

    install protos package

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 # source .venv/bin/activate
 
 # Install projects (editable)
-pip install -e ./buildserver-api[test] -e ./buildserver-runner[test]
+pip install -e ./protos/protos -e ./buildserver-api[test] -e ./buildserver-runner[test]
 
 # Install dev tools
 pip install pylint pre-commit grpcio-tools
