24a704e740ca033651149c5516bb0a2bd2f9d2a9
Larry B <larrykbanks21@gmail.com>
2026-03-08 21:28:19 +0000

    runner registration - registry service - grpc reflection - added runner route

--- buildserver-api/buildserver/main.py
+++ buildserver-api/buildserver/main.py
@@ -9,20 +9,30 @@ import grpc
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from grpc_reflection.v1alpha import reflection
+from protos import registry_pb2, registry_pb2_grpc
 
 from buildserver.api.jobs.views import router as build_router
+from buildserver.api.runners.views import router as runners_router
 from buildserver.config import GRPC_PORT, LOG_LEVEL
 from buildserver.database.core import init_db
 from buildserver.rebuilder import run as run_rebuilder
+from buildserver.api.registry.service import Registry
 
-logging.basicConfig()
+logging.basicConfig(level=LOG_LEVEL, force=True)
 logger = logging.getLogger(__name__)
-logger.setLevel(LOG_LEVEL)
 
 
 def _create_grpc_server() -> grpc.Server:
     server = grpc.server(ThreadPoolExecutor(max_workers=10))
-    # TODO: register service handlers here
+    registry_pb2_grpc.add_RegistryServicer_to_server(Registry(), server)
+    reflection.enable_server_reflection(
+        (
+            registry_pb2.DESCRIPTOR.services_by_name["Registry"].full_name,
+            reflection.SERVICE_NAME,
+        ),
+        server,
+    )
     server.add_insecure_port(f"[::]:{GRPC_PORT}")
     return server
 
@@ -53,6 +63,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.include_router(build_router)
+app.include_router(runners_router)
 
 
 def main():  # noqa: C0116
