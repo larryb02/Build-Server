a7c70b2b1686ce8941ed573eac7b6f676ebd932f
Larry B <larrykbanks21@gmail.com>
2026-03-08 21:27:19 +0000

    create a context manager for grpc services

--- buildserver-api/buildserver/database/core.py
+++ buildserver-api/buildserver/database/core.py
@@ -1,17 +1,24 @@
 """Database engine, session, and base model configuration"""
 
-from typing import Annotated
+from contextlib import contextmanager
+from typing import Annotated, Generator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker, scoped_session, Session
 from fastapi import Depends
 from buildserver.config import DATABASE_URI
 
-engine = create_engine(DATABASE_URI)
+engine = create_engine(
+    DATABASE_URI,
+    pool_size=10,
+    max_overflow=20,
+    pool_pre_ping=True,
+    pool_recycle=1800,
+)
 session_factory = sessionmaker(bind=engine)
 SessionLocal = scoped_session(session_factory)
 
 
-def get_session():
+def get_session() -> Generator[Session, None, None]:
     with SessionLocal() as session:
         try:
             yield session
@@ -23,11 +30,8 @@ def get_session():
             session.close()
 
 
-def create_session() -> scoped_session[Session]:
-    return SessionLocal()
-
-
-DbSession = Annotated[scoped_session, Depends(get_session)]
+session_context = contextmanager(get_session)
+DbSession = Annotated[Session, Depends(get_session)]
 
 
 class Base(DeclarativeBase):
@@ -36,6 +40,6 @@ class Base(DeclarativeBase):
 
 def init_db():
     """Create all database tables."""
-    from buildserver.api.jobs.models import Job, Artifact  # noqa: F401
+    import buildserver.database.models  # noqa: F401
 
     Base.metadata.create_all(bind=engine)
