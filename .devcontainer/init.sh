2cf4c92a0bd73157521a68c5d49690259053de09
Larry Banks <larrykbanks21@gmail.com>
2026-03-28 11:01:19 +0000

    housekeeping

--- .devcontainer/init.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-# python3 -m venv init.venv
-# source init.venv/bin/activate
-python3 -m pip install -r .devcontainer/requirements.txt
-ansible-playbook infra/ansible/deploy_buildserver/site.yml --tags dev -e cluster_namespace=devpod
-# deactivate
